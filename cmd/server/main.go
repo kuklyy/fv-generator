2c0d7af6936b86b5231f8be4893aca7aefbb24dd
Jakub Mikłasz <jakubmiklasz9@gmail.com>
2024-04-21 15:32:37 +0200

    add request logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	slog.Info("starting server")
-	r := chi.NewRouter()
 
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
